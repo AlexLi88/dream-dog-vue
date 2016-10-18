be16f74ceb815b7800daf1f1e1ca9f8b2e010eff
AlexLi88 <alexlxt88@gmail.com>
2016-10-18 15:51:18 -0700

    update phone css

--- src/components/resetpw/resetpw.vue
+++ src/components/resetpw/resetpw.vue
@@ -6,7 +6,7 @@
         ps: '',
         confirmPs: '',
         key: this.$route.query.key,
-        resetSuccess: false,
+        resetSuccess: true,
         lock: require('../../resource/lock_icon.png')
       }
     },
@@ -27,7 +27,9 @@
       submitForm: function(){
         var self = this;
         if(this.ps != this.confirmPs) {
-          document.getElementById('ps-confirm').setCustomValidity("Passwords do not Match");
+          document.getElementById('ps-confirm').setCustomValidity("Passwords do not match");
+        }else if(this.ps.length < 4 || this.confirmPs.length < 4){
+          document.getElementById('ps-confirm').setCustomValidity("Minimum 4 characters");
         }else{
           this.$http.post('http://service.dreamdogapp.com:8080/api/account/reset_password/finish',
             {"key": self.key,
@@ -40,11 +42,13 @@
                if(response.status == 200){
                   self.resetSuccess = true;
                }else{
-                 toastr.error("Errors, try again later");
+                 toastr.error("Some errors happen, please try again later");
                }
-            });
+            }, (err)=>{
+                toastr.error("Some errors happen, please try again later");
+              });
+            }
         }
-      }
     }
   }
 </script>
